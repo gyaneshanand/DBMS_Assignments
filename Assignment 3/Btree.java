ac57ba67b1c6dfa7513ea97d5235e8d49b3deb1a
AMAN ROY <aman16011@iiitd.ac.in>
2018-03-31 20:03:22 +0530

    Final Insertion and Find
    YEAH MRITYU KYA HAI ?

--- Assignment 3/Btree.java
+++ Assignment 3/Btree.java
@@ -38,14 +38,14 @@ class node
 		this.is_leaf = is_leaf;
 	}
 }
-public class Btree
+class Btree1
 {
 	public node root;
 	/*Btree()
 	{
 		root = new node(true , false);
 	}*/
-	public node find(String V)
+	public node find(String V, int query)
 	{
 		node c = root;
 		while(!c.is_leaf)
@@ -71,6 +71,10 @@ public class Btree
 				c = c.pointers.get(index);
 			}
 		}
+		if (query==1)
+		{
+			return c;
+		}
 		for(int i = 0;i < c.values.size();i++)
 		{
 			if(c.values.get(i).compareTo(V) == 0)
@@ -82,13 +86,18 @@ public class Btree
 	}
 	public void insert(String K , String P)
 	{
+		System.out.println(1);
 		if (root == null)
 		{
 			root = new node(true , true);
 			root.data_pointers = new ArrayList<String>();
 			root.data_pointers.add(P);
+			root.values.add(K);
+			root.number_key+=1;
+			return;
 		}
-		node L = find(K);
+		node L = find(K,1);
+		//System.out.println(L);
 		if(L.number_key < L.n) // 5 has to be replaced by n
 		{
 			insert_in_leaf(L,K,P);
@@ -101,7 +110,8 @@ public class Btree
 			{
 				T.values.add(L.values.get(i)); 
 			}
-			for(int i=0;i<L.pointers.size();i++)
+			T.data_pointers = new ArrayList<String>();
+			for(int i=0;i<L.data_pointers.size();i++)
 			{
 				T.data_pointers.add(L.data_pointers.get(i)); 
 			}
@@ -114,16 +124,22 @@ public class Btree
 				L.values.add(T.values.get(i));
 				L.data_pointers.add(T.data_pointers.get(i));
 				L.number_key = (L.n+1)/2;
+				//L.is_leaf = true;
+				//L.is_root = false;
 			}
+			L1.data_pointers = new ArrayList<String>();
 			for(int i= (L.n+1)/2;i<=L.n;i++)
 			{
 				L1.data_pointers.add(T.data_pointers.get(i));
 				L1.values.add(T.values.get(i));
 				L1.number_key = L.n-((L.n+1)/2)+1;
+				//L1.is_leaf = true;
+				//L1.is_root = true;
 			}
 			String K1 = L1.values.get(0);
 			insert_in_parent(L,K1,L1);
 		}
+		print(root);
 	}
 	public void insert_in_leaf(node L , String K , String P)
 	{
@@ -142,13 +158,40 @@ public class Btree
 				{
 					break;
 				}
-				i++;
 			}
 			L.values.add(i,K);
 			L.data_pointers.add(i,P);
+			L.number_key+=1;
 		}
 	}
+	public void print(node n)
+	{
+		if(n==null)
+		{
+			return;
+		}
+		if(n.is_leaf)
+		{
+			for(int i=0;i<n.data_pointers.size();i++)
+			{
+				System.out.print(n.data_pointers.get(i) + " ");
+			}
+			System.out.println();	
+		}
+		else
+		{	
+			for(int i=0;i<n.values.size();i++)
+			{
+				System.out.print(n.values.get(i) + " ");
+			}
+			System.out.println();
+			for(int i=0;i<n.pointers.size();i++)
+			{
+				print(n.pointers.get(i));
+			}
+		}
 
+	}
 	public void insert_in_parent(node N , String K1 , node N1)
 	{
 		if(N.is_root)
@@ -156,11 +199,12 @@ public class Btree
 			node R = new node(true,false);
 			R.pointers.add(N);
 			R.pointers.add(N1);
-			R.number_key+=2;
+			R.number_key+=1;
 			R.values.add(K1);
 			N.parent = R;
 			N.is_root = false;
 			N1.parent = R;
+			this.root = R;
 			return;
 		}
 		node P = N.parent;
@@ -182,8 +226,17 @@ public class Btree
 		else
 		{
 			node T = new node(false,false);
-			T.values = P.values;
-			T.pointers = P.pointers;
+			for(int i=0;i<P.values.size();i++)
+			{
+				T.values.add(P.values.get(i)); 
+			}
+			T.pointers = new ArrayList<node>();
+			for(int i=0;i<P.pointers.size();i++)
+			{
+				T.pointers.add(P.pointers.get(i)); 
+			}
+			//T.values = P.values;
+			//T.pointers = P.pointers;
 			int i;
 			for(i=0;i<T.pointers.size();i++)
 			{
@@ -196,27 +249,45 @@ public class Btree
 			T.pointers.add(i+1,N1);
 			P.values = new ArrayList <String>();
 			P.pointers = new ArrayList <node>();
-			for(int j=0;j<(P.n+1)/2;j++)
+			for(int j=0;j<=(P.n+1)/2;j++)
 			{
 				P.pointers.add(T.pointers.get(j));
 			}
 			P.number_key = (P.n+1)/2;
-			for(int j=0;j<((P.n+1)/2)-1;j++)
+			for(int j=0;j<((P.n+1)/2);j++)
 			{
 				P.values.add(T.values.get(j));
 			}
 			node P1 = new node(false,false);
-			String K2 = T.values.get((P.n+1)/2);
-			for(int j=(P.n+1)/2;j<=P.n;j++)
+			String K2 = T.values.get(((P.n+1)/2));
+			for(int j=(P.n+1)/2+1;j<=P.n+1;j++)
 			{
-				P1.pointers.add(T.pointers.get(i));
+				P1.pointers.add(T.pointers.get(j));
 			}
 
-			for(int j=(P.n+1)/2;j<P.n;j++)
+			for(int j=((P.n+1)/2);j<=P.n;j++)
 			{
-				P1.values.add(T.values.get(i));
+				P1.values.add(T.values.get(j));
 			}
 			insert_in_parent(P,K2,P1);
 		}
+	}
+}
+
+public class Btree
+{
+	public static void main(String [] args) throws IOException
+	{
+		Btree1 b1 = new Btree1();
+		b1.insert("man","parag");
+		b1.insert("aman","parag1");
+		b1.insert("amana","parag2");
+		b1.insert("amanb","parag3");
+		b1.insert("amanc","parag4");
+		b1.insert("ama","parag5");
+		b1.insert("am","parag6");
+		b1.insert("m","parag7");
+		b1.insert("aman10","parag8");
+		b1.insert("aman11","parag9");
 	}
 }
